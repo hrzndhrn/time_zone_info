8ba91124b60ebb35f2967bc0ff1366fb85dd60bd
Marcus Kruse <esurk.sucram@gmail.com>
2020-04-20 19:15:59 +0200

    Add DataPersitence.FileSystem

---
+++ lib/time_zone_info/data_persistence/file_system.ex
@@ -0,0 +1,83 @@
+defmodule TimeZoneInfo.DataPersistence.FileSystem do
+  @moduledoc """
+  An implementation for the behaviour `TimeZoneInfo.DataPersistence` to persist
+  data in the file system.
+  """
+
+  alias File.Stat
+  alias TimeZoneInfo.ExternalTermFormat
+
+  @behaviour TimeZoneInfo.DataPersistence
+
+  @impl true
+  def put(data) do
+    with {:ok, path} <- fetch_path(),
+         {:ok, data} <- ExternalTermFormat.encode(data) do
+      File.write(path, data)
+    end
+  end
+
+  @impl true
+  def fetch do
+    with {:ok, path} <- fetch_path(),
+         {:ok, data} <- File.read(path) do
+      ExternalTermFormat.decode(data)
+    end
+  end
+
+  @impl true
+  def checksum do
+    with {:ok, path} <- fetch_path(),
+         {:ok, data} <- File.read(path) do
+      ExternalTermFormat.checksum(data)
+    end
+  end
+
+  @impl true
+  def fetch_last_update do
+    with {:ok, path} <- fetch_path(),
+         {:ok, %Stat{mtime: mtime}} <- File.stat(path, time: :posix) do
+      {:ok, mtime}
+    end
+  end
+
+  @impl true
+  def put_last_update(time) do
+    with {:ok, path} <- fetch_path() do
+      File.touch(path, time)
+    end
+  end
+
+  @impl true
+  def info do
+    with {:ok, path} <- fetch_path(),
+         {:ok, stat} <- File.stat(path),
+         {:ok, data} <- File.read(path) do
+      %{
+        stat: stat,
+        path: path,
+        checksum: ExternalTermFormat.checksum(data)
+      }
+    end
+  end
+
+  @spec fetch_path ::
+          {:ok, Path.t()} | {:error, {:invalid_config, Keyword.key() | [Keyword.key()]}}
+  defp fetch_path do
+    with {:file_system, {:ok, file_system}} <-
+           {:file_system, Application.fetch_env(:time_zone_info, :file_system)},
+         {:path, {:ok, path}} when is_binary(path) <-
+           {:path, Keyword.fetch(file_system, :path)} do
+      {:ok, path}
+    else
+      {:file_system, :error} ->
+        {:error, {:invalid_config, :file_system}}
+
+      {:path, :error} ->
+        {:error, {:invalid_config, [:file_system, :path]}}
+
+      {:path, {:ok, path}} ->
+        {:error, {:invalid_config, [file_system: [path: path]]}}
+    end
+  end
+end
