0adb1816460507c436fbd0f5afef4825f10a3534
Marcus Kruse <esurk.sucram@gmail.com>
2020-04-22 20:54:54 +0200

    Add tests

---
+++ test/time_zone_info/utc_datetime_test.exs
@@ -0,0 +1,49 @@
+defmodule TimeZoneInfo.UtcDateTimeTest do
+  use ExUnit.Case, async: true
+
+  import TimeZoneInfo.TestUtils
+
+  alias TimeZoneInfo.UtcDateTime
+
+  defmodule UtcFixDateTime do
+    @behaviour TimeZoneInfo.UtcDateTime
+
+    @impl true
+    def now(:datetime), do: fix()
+    def now(:unix), do: DateTime.to_unix(fix())
+
+    def fix, do: ~U[2000-01-01 00:00:00Z]
+  end
+
+  describe "now/1" do
+    test "returns now as datetime" do
+      now = DateTime.utc_now()
+
+      assert_in_delta(
+        DateTime.to_unix(now),
+        UtcDateTime.now() |> DateTime.to_unix(),
+        1
+      )
+    end
+
+    test "returns now as unic" do
+      now = DateTime.utc_now()
+      assert_in_delta(DateTime.to_unix(now), UtcDateTime.now(:unix), 1)
+    end
+  end
+
+  describe "now/1 with UtcFixDateTime" do
+    setup do
+      put_env(utc_datetime: UtcFixDateTime)
+      on_exit(&delete_env/0)
+    end
+
+    test "retruns fixed datetime" do
+      assert DateTime.diff(UtcFixDateTime.fix(), UtcDateTime.now()) == 0
+    end
+
+    test "returns fixed datetime as unix" do
+      assert UtcFixDateTime.fix() |> DateTime.to_unix() == UtcDateTime.now(:unix)
+    end
+  end
+end
