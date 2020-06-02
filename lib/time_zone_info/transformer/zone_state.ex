6ad833445923c147fc25eb11cbba492a6a6993e9
Marcus Kruse <esurk.sucram@gmail.com>
2020-06-02 22:59:35 +0200

    Fix warning

--- lib/time_zone_info/transformer/zone_state.ex
+++ lib/time_zone_info/transformer/zone_state.ex
@@ -3,7 +3,7 @@ defmodule TimeZoneInfo.Transformer.ZoneState do
 
   # The transformer for time-zones.
 
-  alias TimeZoneInfo.{GregorianSeconds, IanaDateTime, IanaParser, Transformer}
+  alias TimeZoneInfo.{GregorianSeconds, IanaDateTime, IanaParser}
   alias TimeZoneInfo.Transformer.{Abbr, Rule, RuleSet}
 
   @end_of_time GregorianSeconds.from_naive(~N[9999-12-31 00:00:00])
