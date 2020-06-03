6eb49157d578b15da831bd142550587074ec7701
Marcus Kruse <esurk.sucram@gmail.com>
2020-06-03 21:18:15 +0200

    Update .dialyzer_ignore.exs

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,5 +1,6 @@
-# See: https://github.com/jeremyjh/dialyxir/issues/369
 [
+  {":0:unknown_function Function :persistent_term.get/0 does not exist."},
   {":0:unknown_function Function :persistent_term.get/2 does not exist."},
-  {":0:unknown_function Function :persistent_term.put/2 does not exist."}
+  {":0:unknown_function Function :persistent_term.put/2 does not exist."},
+  {":0:unknown_function Function :persistent_term.erase/1 does not exist."}
 ]
