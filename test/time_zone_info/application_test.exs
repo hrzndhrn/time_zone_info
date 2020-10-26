6bf5e41b31d4130563b94c8bc34daaf6ffa56f43
Marcus Kruse <esurk.sucram@gmail.com>
2020-10-26 20:05:51 +0100

    Fix test

--- test/time_zone_info/application_test.exs
+++ test/time_zone_info/application_test.exs
@@ -1,7 +1,39 @@
 defmodule TimeZoneInfo.ApplicationTest do
   use ExUnit.Case
 
+  import TimeZoneInfo.TestUtils
+
   test "start" do
+    put_env(
+      files: [
+        "africa",
+        "antarctica",
+        "asia",
+        "australasia",
+        "backward",
+        "etcetera",
+        "europe",
+        "northamerica",
+        "southamerica"
+      ],
+      time_zones: :all,
+      lookahead: 15,
+      data_store: :detect,
+      update: :disabled,
+      listener: TimeZoneInfo.Listener.ErrorLogger,
+      downloader: [
+        module: TimeZoneInfo.Downloader.Mint,
+        uri: "https://data.iana.org/time-zones/tzdata-latest.tar.gz",
+        mode: :iana,
+        headers: [
+          {"content-type", "application/gzip"},
+          {"user-agent", "Elixir.TimeZoneInfo.Mint"}
+        ]
+      ],
+      data_persistence: TimeZoneInfo.DataPersistence.Priv,
+      priv: [path: "data.etf"]
+    )
+
     assert {:ok, _} = Application.ensure_all_started(:time_zone_info)
 
     assert TimeZoneInfo.TimeZoneDatabase.time_zone_periods_from_wall_datetime(
