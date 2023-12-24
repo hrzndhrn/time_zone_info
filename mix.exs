64753ff8902c87c257ca175b6b3b26e86a34ccdf
Marcus Kruse <esurk.sucram@gmail.com>
2023-12-24 11:11:09 +0100

    Releases 0.7.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TimeZoneInfo.MixProject do
   def project do
     [
       app: :time_zone_info,
-      version: "0.7.0",
+      version: "0.7.1",
       elixir: "~> 1.12",
       name: "TimeZoneInfo",
       description: description(),
