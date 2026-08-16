67a6de0fc9ac6e62b44a868649caac58f4311a21
Aaron Seigo <aseigo@mykolab.com>
2026-08-16 18:25:14 +0200

    avoid control flow and calls to the Map module
    this improves performance noticeably in perf profiling and while perhaps
    not as "pretty", this is an internal "hot path" so should be as fast
    as reasonably possible

--- lib/time_zone_info/data_store/persistent_term.ex
+++ lib/time_zone_info/data_store/persistent_term.ex
@@ -6,71 +6,78 @@ defmodule TimeZoneInfo.DataStore.PersistentTerm do
 
   @behaviour TimeZoneInfo.DataStore
 
-  @key {:time_zone_info, :data}
+  @time_zones_key {:time_zone_info, :time_zones}
+  @rules_key {:time_zone_info, :rules}
+  @links_key {:time_zone_info, :links}
+  @version_key {:time_zone_info, :version}
 
   @impl true
   def put(data) do
-    data = Map.merge(%{time_zones: %{}, rules: %{}, links: %{}}, data)
-    :persistent_term.put(@key, data)
+    data =
+      Map.merge(%{time_zones: %{}, rules: %{}, links: %{}, version: "unknown"}, data)
+
+    :persistent_term.put(@time_zones_key, data.time_zones)
+    :persistent_term.put(@rules_key, data.rules)
+    :persistent_term.put(@links_key, data.links)
+    :persistent_term.put(@version_key, data.version)
   end
 
   @impl true
   def fetch_transitions(time_zone, is_link \\ false) do
-    with :error <- fetch(:time_zones, time_zone) do
+    with :error <- fetch(@time_zones_key, time_zone) do
       fetch_transitions_by_link(time_zone, is_link)
     end
   end
 
   @impl true
   def fetch_rules(rules) do
-    with :error <- fetch(:rules, rules) do
+    with :error <- fetch(@rules_key, rules) do
       {:error, :rules_not_found}
     end
   end
 
   @impl true
   def get_time_zones(links: :ignore) do
-    case fetch(:time_zones) do
-      {:ok, time_zones} -> time_zones |> Map.keys() |> Enum.sort()
-      :error -> []
-    end
+    fetch(@time_zones_key) |> Map.keys() |> Enum.sort()
   end
 
   def get_time_zones(links: :only) do
-    case fetch(:links) do
-      {:ok, links} -> links |> Map.keys() |> Enum.sort()
-      :error -> []
-    end
+    fetch(@links_key) |> Map.keys() |> Enum.sort()
   end
 
   def get_time_zones(links: :include) do
-    with {:ok, time_zones} <- fetch(:time_zones),
-         {:ok, links} <- fetch(:links) do
-      Enum.sort(Map.keys(time_zones) ++ Map.keys(links))
-    else
-      _error -> []
-    end
+    time_zones = fetch(@time_zones_key)
+    links = fetch(@links_key)
+    Enum.sort(Map.keys(time_zones) ++ Map.keys(links))
   end
 
   @impl true
-  def empty?, do: :persistent_term.get(@key, nil) == nil
+  def empty?, do: :persistent_term.get(@time_zones_key, nil) == nil
 
   @impl true
-  def version, do: get(:version)
+  def version, do: get(@version_key)
 
   @impl true
   def delete! do
-    :persistent_term.erase(@key)
+    :persistent_term.erase(@time_zones_key)
+    :persistent_term.erase(@rules_key)
+    :persistent_term.erase(@links_key)
+    :persistent_term.erase(@version_key)
     :ok
   end
 
   @impl true
   def info do
     memory =
-      case :persistent_term.get(@key, nil) do
-        nil -> 0
-        data -> data |> :erlang.term_to_binary() |> byte_size()
-      end
+      Enum.sum_by(
+        [@time_zones_key, @rules_key, @links_key],
+        fn key ->
+          case :persistent_term.get(key, nil) do
+            nil -> 0
+            data -> data |> :erlang.term_to_binary() |> byte_size()
+          end
+        end
+      )
 
     %{
       version: version(),
@@ -81,29 +88,27 @@ defmodule TimeZoneInfo.DataStore.PersistentTerm do
   end
 
   defp get(key) do
-    case :persistent_term.get(@key, :error) do
-      :error -> nil
-      data -> Map.get(data, key)
-    end
+    :persistent_term.get(key)
+  rescue
+    _ -> %{}
   end
 
   defp fetch(key) do
-    case :persistent_term.get(@key, :error) do
-      :error -> :error
-      data -> Map.fetch(data, key)
-    end
+    :persistent_term.get(key)
+  rescue
+    _ -> %{}
   end
 
   defp fetch(key, sub_key) do
-    with {:ok, map} <- fetch(key) do
-      Map.fetch(map, sub_key)
-    end
+    key
+    |> fetch()
+    |> Map.fetch(sub_key)
   end
 
   defp fetch_transitions_by_link(_link, true), do: {:error, :transitions_not_found}
 
   defp fetch_transitions_by_link(link, false) do
-    case fetch(:links, link) do
+    case fetch(@links_key, link) do
       {:ok, time_zone} -> fetch_transitions(time_zone, true)
       :error -> {:error, :transitions_not_found}
     end
